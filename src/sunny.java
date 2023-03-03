8108eb87a665f49f22f0c9c15dc96898d1799832
mukeshkadian <mukeshkadianarmy.com>
2023-03-02 20:50:55 -0500

    new thing

--- src/sunny.java
+++ src/sunny.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
 public class sunny {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1st no");
+        int a= sc.nextInt();
+        System.out.println("2nd no6");
+        int b= sc.nextInt();
+        System.out.println("sum of two no " +(a+b));
+    }
 }
